b118a9c4f69fd284c2e407504b8b5bf05bb4d5c5
Utente <Utente@INFO3CATTEDRA>
2023-01-09 11:12:06 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2223_4G_INF_RenziNicolo_ProvaSlitta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
